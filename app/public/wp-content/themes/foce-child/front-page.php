74c438728b716c77e5d67bea895c94ef35d56c57
Charline Vincent <charline.vincent1992@gmail.com>
2024-09-06 10:07:51 +0200

    finalisation projet

--- app/public/wp-content/themes/foce-child/front-page.php
+++ app/public/wp-content/themes/foce-child/front-page.php
@@ -45,7 +45,10 @@ get_header();
 
 
         <section id="studio" class="studio">
-            <h2 class="infade "><span class="infade animtitre">Studio Koukaki</span></h2> <!-- Ajout de class-->
+            <h2 class="infade">
+                <span class="infade animtitre">Studio </span>
+                <span class="infade animtitre koukaki">Koukaki</span>
+            </h2> 
             <div>
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
